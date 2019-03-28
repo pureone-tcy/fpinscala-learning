6b7133739d02eb77fde1d53ba70aaa92c032909a
pureone-tcy <pureone.tcy@gmail.com>
2019-03-28 15:22:39 +0900

    first commit

---
+++ src/main/scala/example/Exercise3.scala
@@ -0,0 +1,33 @@
+package example
+
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  def apply[A](as: A*): List[A] = if(as.isEmpty) Nil else Cons(as.head, apply(as.tail: _*))
+}
+
+class Creature
+class Animal extends Creature
+class Cat extends Animal
+class Container[-T] {
+  def foo[A <: T](t: A): A = new A
+}
+
+object Exercise3 {
+  def main(args: Array[String]): Unit = {
+    val c: Container[Animal] = new Container[Creature]
+  }
+}
