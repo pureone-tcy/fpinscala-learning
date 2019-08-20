0f71e6131078a5e9c6e09ecc9b74e0b84f664406
pureone-tcy <pureone.tcy@gmail.com>
2019-08-20 20:45:11 +0900

    Update Ex6

--- src/main/scala/example/exercise6/Exercise6.scala
+++ src/main/scala/example/exercise6/Exercise6.scala
@@ -149,9 +149,6 @@ object RNG {
         unit(f(a, b))
       }
     }
-
-
-
 }
 
 import State._
@@ -159,12 +156,17 @@ import State._
 case class State[S,+A](run: S => (A, S)) {
 
   // Exercise 6-10
-  def map[B](f: A => B): State[S,B] = ???
+  def map[B](f: A => B): State[S,B] =
+    flatMap(a => unit(f(a)))
 
-  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] = ???
+  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    flatMap(a => sb.map(b => f(a, b)))
 
-  def flatMap[B](f: A => State[S,B]): State[S,B] = ???
-
+  def flatMap[B](f: A => State[S,B]): State[S,B] =
+    State { s =>
+      val (a, s1) = run(s)
+      f(a).run(s1)
+    }
 }
 
 object State {
@@ -173,4 +175,10 @@ object State {
 
   def unit[S, A](a: A): State[S, A] =
     State(s => (a, s))
+
+  def sequenceViaFoldRight[S,A](sas: List[State[S, A]]): State[S, List[A]] =
+    sas.foldRight(unit[S, List[A]](List()))((f, acc) => f.map2(acc)(_ :: _))
+
+  def sequenceViaFoldLeft[S,A](l: List[State[S, A]]): State[S, List[A]] =
+    l.reverse.foldLeft(unit[S, List[A]](List()))((acc, f) => f.map2(acc)( _ :: _ ))
 }
