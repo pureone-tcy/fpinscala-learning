2a94572b2510bea604a105ef7a98c3901cb1904c
pureonedoya <pureonedoya@gmail.com>
2019-10-08 01:59:30 +0900

    Update Tree

--- src/main/scala/com/github/pureone/datastructures/Tree.scala
+++ src/main/scala/com/github/pureone/datastructures/Tree.scala
@@ -1,27 +1,49 @@
 package com.github.pureone.datastructures
 
 sealed trait Tree[+A] {
-  val t = Tree
+  private val t = Tree
   def size: Int = t.size(this)
+  def _size: Int = t._size(this)
   def depth: Int = t.depth(this)
+  def _depth: Int = t.depth(this)
+  def map[B](f: A => B): Tree[B] = t.map(this)(f)
+  def _map[B](f: A => B): Tree[B] = t._map(this)(f)
 }
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
   def size[A](t: Tree[A]): Int = t match {
-    case Leaf(_) => 1
+    case Leaf(_)      => 1
     case Branch(l, r) => 1 + size(l) + size(r)
   }
 
   def maximum[A](t: Tree[Int]): Int = t match {
     case Branch(l, r) => maximum(l) max maximum(r)
-    case Leaf(n) => n
+    case Leaf(n)      => n
   }
 
   def depth[A](t: Tree[A]): Int = t match {
     case Branch(l, r) => 1 + (depth(l) max depth(r))
-    case Leaf(_) => 1
+    case Leaf(_)      => 1
   }
 
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+    case Leaf(v)      => Leaf(f(v))
+  }
+
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(v)      => f(v)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+
+  def _size[A](t: Tree[A]): Int = fold(t)(_ => 1)(1 + _ + _)
+
+  def _maximum[A](t: Tree[Int]): Int = fold(t)(a => a)(_ max _)
+
+  def _depth[A](t: Tree[A]): Int = fold(t)(_ => 1)(_ max _)
+
+  def _map[A, B](t: Tree[A])(f: A => B): Tree[B] =
+    fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_, _))
 }
--- src/test/scala/com/github/pureone/datastructures/TreeSpec.scala
+++ src/test/scala/com/github/pureone/datastructures/TreeSpec.scala
@@ -5,26 +5,37 @@ import org.scalatest.FreeSpec
 class TreeSpec extends FreeSpec {
 
   val tree: Tree[Int] =
-    Branch(
-      Branch(Leaf(1), Leaf(2)),
-      Branch(Leaf(3),
-        Branch(Leaf(4), Leaf(5))))
+    Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Branch(Leaf(4), Leaf(5))))
 
   "size" - {
     "Tree size." in {
       assert(9 == tree.size)
+      assert(9 == tree._size)
     }
   }
 
   "maximum" - {
     "max num." in {
       assert(5 == Tree.maximum(tree))
+      assert(5 == Tree._maximum(tree))
     }
   }
 
   "depth" - {
     "depth num." in {
       assert(4 == tree.depth)
+      assert(4 == tree._depth)
+    }
+  }
+
+  "map" - {
+    "twice." in {
+      val expect: Tree[Int] =
+        Branch(Branch(Leaf(2), Leaf(4)),
+               Branch(Leaf(6), Branch(Leaf(8), Leaf(10))))
+
+      assert(expect == tree.map(_ * 2))
+      assert(expect == tree._map(_ * 2))
     }
   }
 }
