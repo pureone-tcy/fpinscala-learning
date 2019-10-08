2923e9d2d1af3c2b6a1c6795f9bfe09be716d918
pureonedoya <pureonedoya@gmail.com>
2019-10-08 16:59:27 +0900

    Update Option

--- src/main/scala/com/github/pureone/errorhandling/Option.scala
+++ src/main/scala/com/github/pureone/errorhandling/Option.scala
@@ -24,9 +24,29 @@ trait Option[+A] {
 
   def filter(f: A => Boolean): Option[A] =
     flatMap(a => if (f(a)) Some(a) else None)
-
-  def variance(xs: Seq[Double]): Option[Double] = ???
-
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
+
+object Option {
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+
+  def lift[A, B](f: A => B): Option[A] => Option[B] = _ map f
+
+  val abs: Option[Double] => Option[Double] = lift(math.abs)
+
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a flatMap (aa => b map (f(aa, _)))
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+    traverse(a)(x => x)
+
+  private def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a.foldRight[Option[List[B]]](Some(Nil))((h, t) => map2(f(h), t)(_ :: _))
+
+}
